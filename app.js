5393aba48ba8073e640a6f273a5e601e7f00a3f7
Quipildor-Claudio <claudioq00@gmail.com>
2024-01-22 16:41:48 -0300

    node js

--- app.js
+++ app.js
@@ -4,8 +4,6 @@ let path = require('node:path');
 app.get('/cart', (req, res) => {
     res.sendFile(path.join( __dirname,'views/cart.html'));
 });
-app.get('/', (req, res) => {
-    res.sendFile(path.join( __dirname,'views/index.html'));
-});
+
 app.use(express.static('public'))
 app.listen(3000,()=>console.log('Server up : http://localhost:3000'));
